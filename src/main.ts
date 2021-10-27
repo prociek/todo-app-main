cf1d80cd036fa7b3be0e3330e03d3b311691e0d9
Prociek <prociek@poczta.onet.pl>
2021-10-27 22:25:56 +0200

    Add singelton state

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,95 @@
+import { v4 as uuid } from "uuid";
+
 import sunIcon from "./images/icon-sun.svg";
 import moonIcon from "./images/icon-moon.svg";
 
 class ToggleMode {
   button: HTMLButtonElement;
+  image: HTMLImageElement;
   sunIcon: string;
   moonIcon: string;
 
   constructor(sunIcon: string, moonIcon: string) {
     this.button = document.querySelector(
       ".header__button"
-    )! as HTMLButtonElement;
+    ) as HTMLButtonElement;
+    this.image = this.button.querySelector(".header__icon") as HTMLImageElement;
     this.sunIcon = sunIcon;
     this.moonIcon = moonIcon;
-    this.configure();
+    this.config();
   }
 
-  configure() {
+  config() {
     this.button.addEventListener("click", this.handleClick);
   }
 
   handleClick = () => {
     if (!document.documentElement.hasAttribute("data-theme")) {
       document.documentElement.setAttribute("data-theme", "dark");
-      this.button
-        .querySelector(".header__icon")
-        .setAttribute("src", this.sunIcon);
+      this.image.setAttribute("src", this.sunIcon);
     } else {
       document.documentElement.removeAttribute("data-theme");
-      this.button
-        .querySelector(".header__icon")
-        .setAttribute("src", this.moonIcon);
+      this.image.setAttribute("src", this.moonIcon);
     }
   };
 }
 
+class TodoForm {
+  form: HTMLFormElement;
+  input: HTMLInputElement;
+
+  constructor() {
+    this.form = document.querySelector(".todo-form") as HTMLFormElement;
+    this.input = this.form.querySelector("input") as HTMLInputElement;
+    this.config();
+  }
+
+  config() {
+    this.form.addEventListener("submit", this.addTodo);
+  }
+
+  addTodo = (e) => {
+    e.preventDefault();
+    // Add todo to state
+    const text = this.input.value;
+    state.addTodo({ id: uuid(), text, completed: false });
+  };
+}
+
+type Todo = {
+  id: string;
+  text: string;
+  completed: boolean;
+};
+
+type Listener = (todoList: Todo[]) => void;
+
+class TodosState {
+  private static instance: TodosState;
+  private listeners: Listener[] = [];
+  private todos: Todo[] = [];
+
+  private constructor() {}
+
+  public static getInstance(): TodosState {
+    if (!TodosState.instance) {
+      TodosState.instance = new TodosState();
+    }
+    return TodosState.instance;
+  }
+
+  public subscibeListener(listener: Listener) {
+    this.listeners.push(listener);
+  }
+
+  public addTodo(todo: Todo) {
+    this.todos.push(todo);
+    for (let listener of this.listeners) {
+      listener(this.todos);
+    }
+  }
+}
+
 new ToggleMode(sunIcon, moonIcon);
+new TodoForm();
+const state = TodosState.getInstance();
